cc8759a8cad90e47dcaace61d52005ee1c565e5f
Claure <cclaurev@est.emi.edu.bo>
2021-06-17 18:19:22 -0400

    debug false

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def exportar():
     return send_file(archivo, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True, threaded=True)
+    app.run(port=3000, threaded=True)
