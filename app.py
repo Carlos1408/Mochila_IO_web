611d2c74b4767a391bd73baa6920e0c29edf3d3e
cclarev <cclarev@est.emi.edu.bo>
2021-06-29 13:09:20 -0400

    Adicion del repositorio de github

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def exportar():
     return send_file(archivo, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(port=3000, threaded=True)
+    app.run(port=3000, debug=True, threaded=True)
